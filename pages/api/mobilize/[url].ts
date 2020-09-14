25272b42785c842a32c2ed13d63b85bd0b9c37d2
Jay Harris <jay.harris@outlook.co.nz>
2020-09-15 11:08:29 +1200

    Sanitize content before returning it

--- pages/api/mobilize/[url].ts
+++ pages/api/mobilize/[url].ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { Readability } from "@mozilla/readability";
 import { JSDOM } from 'jsdom';
+import createDOMPurify from 'dompurify';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     res.statusCode = 200;
@@ -10,8 +11,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const response = await fetch(url);
     const html = await response.text();
     const dom = new JSDOM(html, { url });
+    const DOMPurify = createDOMPurify(dom.window as any);
+
     const reader = new Readability(dom.window.document);
     const article = reader.parse();
+    // Sanitize content for display.
+    article.content = DOMPurify.sanitize(article.content);
 
     if ('includeHtml' in req.query)
         article['html'] = html;
